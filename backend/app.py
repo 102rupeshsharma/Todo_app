5533a269fbcdf15d3dd56d839f3b2de787fc9f42
Rupesh Sharma <102rupeshsharma@gmail.com>
2025-04-10 16:57:52 +0530

    Added database info in dotenv file

--- backend/app.py
+++ backend/app.py
@@ -2,20 +2,20 @@ import traceback
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pymysql
-# import mysql.connector
 import bcrypt
-# import os
+from dotenv import load_dotenv
+import os
 from datetime import datetime, date, time, timedelta
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
-
+load_dotenv()
 
 db_config = {
-    'host':"sql12.freesqldatabase.com",
-    'user':"sql12771449",
-    'password':"KiWMrQblau",
-    'database':"sql12771449"
+    'host': os.getenv('HOST'),
+    'user': os.getenv('USER'),
+    'password': os.getenv('PASSWORD'),
+    'database': os.getenv('DATABASE')
 }
 def connect_db():
     return pymysql.connect(**db_config, cursorclass=pymysql.cursors.DictCursor)
